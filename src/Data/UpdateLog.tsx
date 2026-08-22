bde6df6160c613baa1c7297c4e5c8586303c9d15
BDFFZI <1330958515@qq.com>
2026-08-22 23:57:04 +0800

    -

--- src/Data/UpdateLog.tsx
+++ src/Data/UpdateLog.tsx
@@ -2,6 +2,10 @@ import { ReactNode } from "react";
 
 const Log: { time: string; content: ReactNode }[] = [
   {
+    time: "2026.8.22",
+    content: "老群解散了，重新创建了新群。",
+  },
+  {
     time: "2026.5.7",
     content: "群又被爆破了，com3d这个圈子显然非常不欢迎公开社区，只能用二级群跳转的方式来保持聊天群的存在了，所以重新将老群作为默认入口。",
   },
